8cb6cc21f8cfd69d53201b2e88fc65e41955eb39
suruliaravinth <suruliaravinths@gmail.com>
2020-08-07 23:01:58 +0530

    Latest

--- src/test/java/org/maveric/MavenTraining/Sample.java
+++ src/test/java/org/maveric/MavenTraining/Sample.java
@@ -3,6 +3,7 @@ package org.maveric.MavenTraining;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("This is Aravinth");
 	}
 
 }
